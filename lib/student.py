4378ec08e3422224c5c6bbe587d95d5a1b1e1b46
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-21 10:35:57 +0300

    Define Student class as subclass of User with knowledge and learn method

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-
-from user import User
+from lib.user import User  # use absolute import
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, string):
+        self.knowledge.append(string)
