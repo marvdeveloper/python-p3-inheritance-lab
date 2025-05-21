cc2dc726bad238c3f06b7916a0e996bc96e00e01
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-21 10:36:33 +0300

    Ensure Teacher inherits from User and includes teach method

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
-
-from user import User
-
+from lib.user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Math",
+            "Science",
+            "History",
+            "Literature",
+            "Art"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
